9f446c1b7f81ab2a4c445b2566092c0b7c22920b
Trictonicmp <code_shadow@hotmail.com>
2022-07-26 23:34:00 -0500

    Add Solver class and factorial method

---
+++ solver.rb
@@ -0,0 +1,11 @@
+class Solver
+  def factorial(number)
+    raise Exception.new("Not a valid value") if number < 0
+    return factorial_calculator(number)
+  end
+
+  def factorial_calculator(number)
+    return 1 if number == 0
+    return number * factorial_calculator(number - 1)
+  end
+end
